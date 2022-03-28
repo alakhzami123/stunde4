331c8721deabb7c8e95316f63df254e1fdd20b3f
student 04 <stk_lum_04@outlook.de>
2022-03-28 13:16:44 +0200

    hi

---
+++ stunde4/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stunde4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arbeiten mit string.");
+            string wiederholen;
+
+            do
+            {
+                //Menü
+                int auswahl;
+                Console.WriteLine("1: zeichnen ersetzen.");
+                Console.WriteLine("2: Vokale entfernen.");
+                Console.WriteLine("3: Beenden.");
+
+                Console.Write("Auswahl");
+                auswahl = Convert.ToInt32( Console.ReadLine() );
+
+                switch(auswahl)
+                {
+                    case  1:
+                        Console.WriteLine("1: zeichnen ersetzt werden.");
+                        string zuErsetzen, zeichen, ersatz; ;
+                        Console.WriteLine("zeichenkette eingeben:");
+                        zuErsetzen = Console.ReadLine();
+                        Console.WriteLine("Welches Zeichen soll ersetzt werden?");
+                        zeichen = Console.ReadLine();
+                        Console.WriteLine("Wodurch soll" + zeichen + "ersetzt werden?");
+                        ersatz = Console.ReadLine();
+                        Console.WriteLine("Es wird " + zeichen + " durch " + ersatz + " ersetzt:");
+
+                        string ersetzt = "";
+                        ersetzt = zuErsetzen.Replace(zeichen, ersatz);
+                        //for (int i = 0; i < zuErsetzen.Length; i++)
+                        //{
+                        //    if(zuErsetzen[i].ToString() == zeichen )
+                        //    {
+                        //        ersetzt += ersatz;
+                        //    }
+                        //    else
+                        //    {
+                        //        ersetzt += zuErsetzen[i];
+
+                        //    }
+
+                        //}
+                        Console.WriteLine(zuErsetzen + " wird zu " + ersetzt);
+
+
+
+
+
+
+
+                        break;
+                     case 2:
+                        Console.WriteLine("2: Vokale entfernen.");
+                        string zeichenkette;
+                        Console.WriteLine("zeichenkette eingeben:");
+                        zeichenkette = Console.ReadLine();
+                        string ergebnis = "";
+
+
+
+                        Console.WriteLine(zeichenkette + " wird zu " + ergebnis);
+                        break;
+                     case 3:
+                        Console.WriteLine("3:Programm wird beendet.");
+                        
+
+
+                        break;
+
+
+                     default:
+                        Console.WriteLine("Auswahl ungültig.");
+
+
+                        break;
+
+                }
+                Console.WriteLine("Neue Auswahl? (j/n)");
+                wiederholen = Console.ReadLine();
+
+            } while (wiederholen == "j" || wiederholen == "J");
+        }
+    }
+}
---
+++ stunde4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("stunde4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("stunde4")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("feec5f0b-c008-4c95-bf1d-aec63c6558e4")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
